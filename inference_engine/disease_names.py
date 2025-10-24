f89b8c8744364d96d491ea8d77752b8ad8207384
Arditaaura <arditaauraa@gmail.com>
2025-10-24 23:41:26 +0700

    add: file disease_names.py

---
+++ inference_engine/disease_names.py
@@ -0,0 +1,10 @@
+disease_names = {
+    "P01" : "Kanker Payudara Stadium 0",
+    "P02" : "Kanker Payudara Stadium 1", 
+    "P03" : "Kanker Payudara Stadium 2A",
+    "P04" : "Kanker Payudara Stadium 2B", 
+    "P05" : "Kanker Payudara Stadium 3A",
+    "P06" : "Kanker Payudara Stadium 3B",
+    "P07" : "Kanker Payudara Stadium 3C",
+    "P08" : "Kanker Payudara Stadium 4"
+}
